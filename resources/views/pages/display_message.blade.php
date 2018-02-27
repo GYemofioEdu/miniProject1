ad64ad7f60f307f466d874d15aa4417f8fae20c6
Godfred <gny2@njit.edu>
2018-02-27 00:47:35 -0500

    Initial commit

---
+++ resources/views/pages/display_message.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <div class="card mt-3">
+        <h5 class="card-header">From: YieldToEmailAddress@Variable.Com</h5>
+        <h5 class="card-title">Subject: Not sure how to get this yet</h5>
+        <div class="card-body">
+            <p class="card-text">One giant paragraph from the body of the contact message</p>
+            <p class="card-text">What if all text including the break tags? Other HTML?</p>
+            <a href="#" class="btn btn-primary">Back to Messages</a>
+        </div>
+    </div>
+
+@endsection
